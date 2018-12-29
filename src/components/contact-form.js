0d940edeebfa1c2c937280953a6253f60a35483b
rosaxny <rosaxny@gmail.com>
2018-12-29 16:20:23 -0500

    set up redux form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default function ContactForm() {
-    return (
-        <form>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="email">Email address</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" />
-            <button>Submit</button>
-        </form>
-    );
+export class ContactForm extends React.Component {
+    onSubmit(values) {
+        console.log(values)
+    }
+    render() {
+        return (
+            <form
+                onSubmit={this.props.handleSubmit((values) => this.onSubmit(values))}
+            >
+                <label htmlFor="name">Your Name</label>
+                <Field component="input" type="text" id="name" name="name" />
+                <label htmlFor="email">Email address</label>
+                <Field component="input" type="email" id="email" name="email" />
+                <label htmlFor="message">Message</label>
+                <Field component="textarea" id="message" name="message" />
+                <button type="submit" disabled={ this.props.pristine || this.props.submitting }>Submit</button>
+            </form>
+        );
+    }
+    
 }
+
+export default reduxForm({
+    form: 'contact'
+})(ContactForm);
